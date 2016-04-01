8a7a15a3962f5b329d1a6369fff39a9eb4a1a49f
TanyaHurtado <Tanya-Hurtado@hotmail.com>
2016-04-01 14:29:49 -0700

    Update reporte.php

--- reporte.php
+++ reporte.php
@@ -1,50 +1,101 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<title></title>
-	<meta charset="UTF-8"/>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text"/>
-        <script type="text/javascript" src="bootstrap/js/jquery.js"></script>
-        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-	</head>
-	<body>
-				<?php
-				$con=mysqli_connect("localhost","root","lolMYSQL420","bd");
-				if (mysqli_connect_errno())
-				  {
-				  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-				  }
-				$sql="SELECT id,telefono,linea,mensaje,recepcion,campania,registro,tipo FROM entrantes";
-
-				if ($result=mysqli_query($con,$sql))
-				  {
  
-				
-				echo "<table class='table table-striped'>";
-							echo "<tr>";
-							echo "<td><label>ID :</label> </td>";
-							echo "<td><label>Teléfono: </label></td>";
-							echo "<td><label>Línea:</label> </td>";
-							echo "<td><label>Mensaje Inicial:</label> </td>";
-							echo "<td><label>Recepción: </label> </td>";
-							echo "<td><label>Hora Entrada:</label> </td>";
-							echo "<td><label>Campaña:</label> </td>";
-							echo "<td><label>Registro: </label></td>";
-							echo "<td><label>Tipo: </label></td>";
-							echo "<td></td>";
-							echo "</tr>";
-					while ($reg=mysqli_fetch_object($result)){
-							echo "<tr>";
-							
-							echo "<td>$reg->id</td>";
-							echo "<td>$reg->telefono</td>";
-							echo "<td>$reg->linea</td>";	
-							echo "<td>$reg->mensaje</td>";	
-							echo "<td>$reg->recepcion</td>";	
-							echo "<td>$reg->campania</td>";
-							echo "<td>$reg->registro</td>";	
-							
-							switch ($reg->tipo) {
+<html>
+<head> <meta charset="UTF-8"/>
+  <!-- Bootstrap -->
+  <link href="bootstrap/bootstrap.min.css" rel="stylesheet" />
+  <!-- Bootstrap Material Design -->
+  <link href="bootstrap/bootstrap-material-design.css" rel="stylesheet"/>
+
+  </head>
+  <style type="text/css">
+  	.table-hover thead {
+        background-color: #7ECFF5;
+	}
+	  .table-hover tbody {
+	        background-color: white;
+	}
+	 .table-hover tbody tr:hover td {
+	        /*background-color:#20D0D8;*/
+	         background-color: #B1E2F9;
+	}
+	body{
+	  	     background-color: white;
+  }
+  </style>
+  <script type="text/javascript">
+  		$(document).ready(function() {
+	    setTimeout(function() {
+	        $(".alert").alert('close');
+	    }, 2000);
+	});
+  </script>
+ <body>
+ <table class="table table-striped table-hover ">
+ <thead>
+ 	 <tr>
+    <th>ID</td>
+    <th>Teléfono</td>
+    <th>Línea</td>
+    <th>Mensaje</td>
+    <th>Recepción</td>
+    <th>Campaña</td>
+    <th>Registro</td>
+    <th>Tipo</td>
+  </tr>
+ </thead>
+ <tbody>
+ 	
+  <tr class="x">
+    <?php
+		$mysqli = new mysqli("localhost", "root", "", "bd");
+		if ($mysqli->connect_errno) {
+			echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+		}
+		if (is_null($_GET) || !isset($_GET["cuantos"]))
+		{
+			$nn = 50;
+		}
+		else
+		{
+			$nn = $_GET["cuantos"];
+		}
+		$count = mysqli_fetch_assoc($mysqli->query("select count(*) from entrantes"))["count(*)"];
+		if (is_null($_GET) || !isset($_GET["n"]))
+		{
+			$n = $i = 0;
+		}
+		else
+		{
+			$i = $_GET["n"];
+			if ($i != $count - $nn)
+			{
+				$n = $i - ($i % $nn);
+				$i = $n;
+			}
+			else
+			{
+				$n = $i;
+			}
+		}
+		if (!$q = $mysqli->query("select * from entrantes where id > ".$n)) {
+			echo "Query Failed!: (" . $mysqli->errno . ") ". $mysqli->error;
+		}
+		
+		while ($row = mysqli_fetch_array($q)) {
+			$i = $i + 1;
+			if ($i > $nn + $n)
+			{
+				break;
+			}
+			echo "<tr >";
+			echo "<td>".$row[0]."</td>";
+			echo "<td>".$row[1]."</td>";
+			echo "<td>".$row[2]."</td>";
+			echo "<td>".$row[3]."</td>";
+			echo "<td>".$row[4]."</td>";
+			echo "<td>".$row[5]."</td>";
+			echo "<td>".$row[6]."</td>";
+			switch ($row[7]) {
 							    case " ":
 							        $Tipo="No clasificado";
 							        break;
@@ -66,14 +117,39 @@
 							        default:
 							        	 $Tipo="No clasificado";
 							        	 break;
-
 							}
-							echo "<td>$Tipo</td>";				
-						echo "</tr>";
-				}
-				}
-					echo "</table>";
+			echo "<td>".$Tipo."</td>";
 
- ?>
-		 </body>
-		 </html>
+		}
+		$q->free();
+		$mysqli->close();
+	?>
+	</tr>
+	</tbody>
+</table>
+<table>
+	<tr>
+		<td id="tipoResponse"></td>
+	</tr>
+</table>
+<br>
+<ul class='pagination' style='margin-left: 40%;'>
+<?php
+	if ($nn < $count)
+	{
+		echo "<li".((($n - $nn) >= 0) ? "><a href=\"reporte.php?cuantos=".$nn."&n=".(0)."\">&lt;&lt;</a>" : " class=\"page-item disabled\"><a href=\"#\">&lt;&lt;</a>")."</li>";
+		echo "<li".(($n > 0) ? "><a href=\"reporte.php?cuantos=".$nn."&n=".max($n - $nn, 0)."\">&lt;</a>" : " class=\"page-item disabled\"><a href=\"#\">&lt;</a>")."</li>";
+		$i = 1;
+		$j = 0;
+		while ($j < $count)
+		{
+			echo "<li".(($n == $j) ? " class=\"page-item active\"><a href=\"#\">".$i."</a>" : "><a href=\"reporte.php?cuantos=".$nn."&n=".$j."\">".$i."</a>")."</li>";
+			$j = $j + $nn;
+			$i = $i + 1;
+		}
+		echo "<li".((($n + $nn) < $count) ? "><a href=\"reporte.php?cuantos=".$nn."&n=".($n + $nn)."\">&gt;</a>" : " class=\"page-item disabled\"><a href=\"#\">&gt;</a>")."</li>";
+		echo "<li".(($count - $nn != $n) ? "><a href=\"reporte.php?cuantos=".$nn."&n=".max($count - $nn, 0)."\">&gt;&gt;</a>" : " class=\"page-item disabled\"><a href=\"#\">&gt;&gt;</a>")."</li>";
+	}
+?>
+</body>
+</html>
