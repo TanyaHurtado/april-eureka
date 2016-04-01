6140038440a8712e8ac7714b4146555cb6a403e1
DaftPink <luismacario_smash@hotmail.com>
2016-04-01 11:52:55 -0700

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "lolMYSQL420";/*CONTRASEÑA*/
-	$dbname = "bd";
+	$servername = "";
+	$username = "";
+	$password = "";
+	$dbname = "";
 ?>
